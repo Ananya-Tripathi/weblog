6a46169f9b05492d588ae48566595c4c58f718bc
ANANYA TRIPATHI <ananyatrip0802@gmail.com>
2023-04-15 17:43:15 +0530

    mern blog

---
+++ client/src/pages/EditPost.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { Navigate, useParams } from "react-router-dom";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
+
+const EditPost = () => {
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [file, setFile] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  async function updatePost(ev) {
+    ev.preventDefault();
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", file?.[0]);
+    data.set("id", id);
+    if (file?.[0]) {
+      data.set("file", file?.[0]);
+    }
+    const response = await fetch("http://localhost:4000/post", {
+      method: "PUT",
+      body: data,
+      credentials: "include",
+    });
+    if (response.ok) {
+      //   setRedirect(true);
+    }
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:4000/post/" + id).then((response) => {
+      response.json().then((postInfo) => {
+        setTitle(postInfo.title);
+        setContent(postInfo.content);
+        setSummary(postInfo.summary);
+      });
+    });
+  }, []);
+
+  if (redirect) {
+    return <Navigate to={"/post/" + id} />;
+  }
+  return (
+    <form onSubmit={updatePost}>
+      <input
+        type="text"
+        className="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="text"
+        className="summary"
+        placeholder={"summary"}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input type="file" onChange={(ev) => setFile(ev.target.files)} />
+      <ReactQuill
+        value={content}
+        modules={modules}
+        formats={formats}
+        onChange={(newValue) => setContent(newValue)}
+      />
+      <button style={{ marginTop: "5px" }}>Update Post</button>
+    </form>
+  );
+};
+
+export default EditPost;
